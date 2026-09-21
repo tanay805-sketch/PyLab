6eae85199f9ae43b3979d3f35cc35297e2160811
Tanay <tanaysrivastava0805@gmail.com>
2026-09-21 14:46:23 +0530

    added max number between 2 numbers

---
+++ gr8est.py
@@ -0,0 +1,15 @@
+a = int(input("enter the 1st number : "))
+b = int(input("enter the 2nd number : "))
+
+
+def largest(a , b ):
+    if a > b:
+        return "a is the largest"
+        
+        
+    else:
+        return "b is largest" 
+        
+
+
+print(largest(a , b ))
